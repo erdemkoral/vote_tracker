3eb5fedae5108b7ae78c1a524b8f63c9eea61dba
Arielle Foldoe <afoldoe@gmail.com>
2017-03-11 12:31:07 -0800

    chart added

--- vote_tracker/vote_tracker.js
+++ vote_tracker/vote_tracker.js
@@ -110,3 +110,25 @@ createTable();*/
 
 window.addEventListener("load", showImages);
 window.addEventListener("load", makeImagesClickable);
+
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "My First Chart in CanvasJS"              
+		},
+		data: [              
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "column",
+			dataPoints: [
+				{ label: "apple",  y: 10  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 25  },
+				{ label: "mango",  y: 30  },
+				{ label: "grape",  y: 28  }
+			]
+		}
+		]
+	});
+	chart.render();
+}
